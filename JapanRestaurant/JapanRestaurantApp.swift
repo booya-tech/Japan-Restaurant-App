50bb8f7a2e2936892d527ab0f1fad3324aaef8c3
booya-tech <booya.pannachai@gmail.com>
2023-06-19 11:14:16 +0700

    Initial Commit

---
+++ JapanRestaurant/JapanRestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  JapanRestaurantApp.swift
+//  JapanRestaurant
+//
+//  Created by Panachai Sulsaksakul on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct JapanRestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
